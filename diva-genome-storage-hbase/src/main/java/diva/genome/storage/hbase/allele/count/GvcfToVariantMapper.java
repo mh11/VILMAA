139fb43440b04e2e5d2f880d2d54207016422e25
Matthias Haimel <mh719@cam.ac.uk>
2017-05-27 10:10:28 +0100

    direct: transfer in ordered position

--- diva-genome-storage-hbase/src/main/java/diva/genome/storage/hbase/allele/count/GvcfToVariantMapper.java
+++ diva-genome-storage-hbase/src/main/java/diva/genome/storage/hbase/allele/count/GvcfToVariantMapper.java
@@ -3,16 +3,15 @@ package diva.genome.storage.hbase.allele.count;
 import diva.genome.storage.hbase.allele.count.region.AlleleRegionCalculator;
 import diva.genome.storage.hbase.allele.count.region.AlleleRegionStore;
 import diva.genome.storage.hbase.allele.count.region.HBaseAlleleRegionTransfer;
+import diva.genome.util.PointRegion;
 import diva.genome.util.Region;
 import org.apache.hadoop.hbase.client.Append;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
-import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by mh719 on 25/05/2017.
@@ -48,17 +47,24 @@ public class GvcfToVariantMapper extends HbaseTableMapper {
     protected Collection<Append> packageAlleleCounts(String chromosome, String studyId, AlleleRegionCalculator alleleCalculator) {
         AlleleRegionStore store = alleleCalculator.getStore();
         Region targetRegion = store.getTargetRegion();
-        this.gethBaseAlleleRegionTransfer().transfer(chromosome, targetRegion, store, (var, cnt) -> {
-            Put put = this.alleleCountToHBaseConverter.convertPut(
-                    var.getChromosome(), var.getStart(), var.getReference(), var.getAlternate(), cnt);
-            try {
-                getContext().write(new ImmutableBytesWritable(put.getRow()), put);
-            } catch (IOException e) {
-                throw new IllegalStateException("Issue submitting " + var, e);
-            } catch (InterruptedException e) {
-                throw new IllegalStateException("Issue submitting " + var, e);
-            }
-        });
+        // get all Variant positions out
+        List<Integer> lst =
+                new ArrayList<>(store.getVariation(targetRegion).stream().map(e -> e.getStart()).collect(Collectors.toSet()));
+        Collections.sort(lst);
+        // Iterate from first to last in order
+        for (Integer position : lst) {
+            this.gethBaseAlleleRegionTransfer().transfer(chromosome, new PointRegion(null, position), store, (var, cnt) -> {
+                Put put = this.alleleCountToHBaseConverter.convertPut(
+                        var.getChromosome(), var.getStart(), var.getReference(), var.getAlternate(), cnt);
+                try {
+                    getContext().write(new ImmutableBytesWritable(put.getRow()), put);
+                } catch (IOException e) {
+                    throw new IllegalStateException("Issue submitting " + var, e);
+                } catch (InterruptedException e) {
+                    throw new IllegalStateException("Issue submitting " + var, e);
+                }
+            });
+        }
         return Collections.emptyList();
     }
 
